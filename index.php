ffd404851e3555fcab9754df1cbbc566b35af3f9
Andy West <captainrex2000@gmail.com>
2015-05-14 14:48:48 -0700

    update :D

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Super smash bros</title>
+        <title>Super Smash Bros</title>
     </head>
     <body>
         <?php
